a1c0933ab78b432bb5a660fb3ee0d3812a68c4af
Oleg <ordo1882@gmail.com>
2023-07-16 19:46:18 +0200

    Edited Task 1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -3,9 +3,44 @@
 // 12821 -> да
 // 23432 -> да
 
-bool IsValidateNumber(int number)
+
+// Первый Вариант:
+//======================================================================
+// bool IsValidateNumber(int number)
+// {
+//     if ((number < 10000) || (number > 99999))
+//     {
+//         System.Console.WriteLine("Need to be only five-digit number");
+//         return false;
+//     }
+//     return true;
+// }
+
+// System.Console.WriteLine("Enter a five-digit number:");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// if (IsValidateNumber(n))
+// {
+//     int num1 = n / 1000; // Разбиваем число на две части исключив при этом среднюю цифру.
+//     int num2 = n % 100;
+
+//     if ((num1 / 10 == num2 %10) && (num1 % 10 == num2 / 10))
+//     {
+//         System.Console.WriteLine("The number is a palindrome");
+//     }
+//     else
+//     {
+//         System.Console.WriteLine("The number is not a palindrome");
+//     }
+// }
+
+
+// Второй вариант:
+//================================================================
+
+bool IsValidateNumber(string num)
 {
-    if ((number < 10000) || (number > 99999))
+    if (num.Length != 5)
     {
         System.Console.WriteLine("Need to be only five-digit number");
         return false;
@@ -13,15 +48,12 @@ bool IsValidateNumber(int number)
     return true;
 }
 
-System.Console.WriteLine("Enter a five-digit number:");
-int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter a five-digit number:");
+string number = Console.ReadLine();
 
-if (IsValidateNumber(n))
+if (IsValidateNumber(number))
 {
-    int num1 = n / 1000; // Разбиваем число на две части исключив при этом среднюю цифру.
-    int num2 = n % 100;
-
-    if ((num1 / 10 == num2 %10) && (num1 % 10 == num2 / 10))
+    if ((number[0] == number[4]) && (number[1] == number[3]))
     {
         System.Console.WriteLine("The number is a palindrome");
     }
